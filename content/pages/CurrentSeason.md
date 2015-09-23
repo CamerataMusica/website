9ce4f0c93f35e3c550dbe6035d8bec27c31e0a4e
colinbrislawn <cbrisl@gmail.com>
2015-09-22 21:06:37 -0700

    use article_list template for CurrentSeason page

--- content/pages/CurrentSeason.md
+++ content/pages/CurrentSeason.md
@@ -1,24 +1,4 @@
 Title: Current Season 
 Date: 2015-07-18
 Authors: Colin Brislawn
-
-# Our Current Season
-
-[![](/images/Brasil200.png){:width="200px", .floatleft}](/BrasilGuitarDuo.html)
-##[The Brasil Guitar Duo](/BrasilGuitar Duo.html)
-#### September 27, 2014 8:00pm at Battelle Auditorium
-
-The Brasil Guitar Duo, winner of the 2006 Concert Artists Guild International Competion comes to the Tri Cities
-
-<hr class="clear"></hr>
-
-![/2014-2015/Onyx Chamber Players.html](/images/OnyxMerge200.jpg){:width="200px", .floatleft}
-
-## Onyx Chamber Players
-#### October 25, 2014 8:00pm at Battelle Auditorium
-
-Onyx Chamber Players Return to Camerata Musica 
-
-<hr class="clear"></hr>
-
-Byron Schenkman & Friends
+Template: article_list
--- pelicanconf.py
+++ pelicanconf.py
@@ -5,11 +5,8 @@ from __future__ import unicode_literals
 AUTHOR = u'Colin Brislawn'
 SITENAME = u'Camerata Musica'
 SITEURL = ''
-
 PATH = 'content'
-
 TIMEZONE = 'America/Los_Angeles'
-
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
@@ -20,8 +17,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = True
-
+DISPLAY_CATEGORIES_ON_MENU = False
 # Blogroll
 #LINKS = (('Pelican', 'http://getpelican.com/'),
 #         ('Python.org', 'http://python.org/'),
@@ -37,16 +33,24 @@ SOCIAL = (
 
 NEWEST_FIRST_ARCHIVES = True
 DEFAULT_PAGINATION = 10
+#ARTICLE_ORDER_BY = 'date'
 ARTICLE_ORDER_BY = 'date'
-PAGE_ORDER_BY = 'date'
-#ARTICLE_ORDER_REVERSED = True
-#REVERSE_CATEGORY_ORDER = False
+NEWEST_FIRST_ARCHIVES = False
+
+PAGE_ORDER_BY = 'reversed-date'
 #LOAD_CONTENT_CACHE = False
+
+
+
+STATIC_PATHS = ['images', 'pages', 'favicon.png', 'CamerataMusica/images']
+
+
+SUMMARY_MAX_LENGTH = 20
+
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 THEME = "./pelican-bootstrap3"
-#THEME = "../pelican-themes/bootstrap"
 #THEME = "../pelican-themes/bootstrap2"
 #THEME = "../pelican-themes/bootstrap2-dark"
 
