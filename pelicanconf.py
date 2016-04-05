15c0bec35d13214092f07a463c6c2effa8d2e6ed
Colin Brislawn <cbrisl@gmail.com>
2016-04-05 16:00:44 -0700

    undo error to test TravisCI

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ TIMEZONE = 'America/Los_Angeles'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = asdf
+FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
