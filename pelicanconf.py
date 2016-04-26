4fe5f74d5ce6cbddef38639ad746427008de302e
Colin Brislawn <cbrisl@gmail.com>
2016-04-25 20:21:36 -0700

    update footer to close #12

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Colin Brislawn'
+AUTHOR = u'Camerata Musica'
 SITENAME = u'Camerata Musica'
 SITEURL = ''
 PATH = 'content'
