7854ec4b29544aa6d9b8dae7ff9a1a53ce24a43e
colinbrislawn <cbrisl@gmail.com>
2016-08-10 20:45:03 -0700

    reverting to github site until we get the DNS A record figured out

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.cameratamusica.com'
+SITEURL = 'https://colinbrislawn.github.io/CamerataMusica/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
